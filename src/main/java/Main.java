94213d21ef357283659237d44f92577fac4c9158
Danilozxz <progdanilo@gmail.com>
2025-03-17 19:52:20 -0300

    feat: Ajuste no main e converter do OffsetDateTime

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,24 @@
-package org.example;
-
 import br.com.desafio.persistence.migration.MigrationStrategy;
 
 import java.sql.SQLException;
 
 import static br.com.desafio.persistence.config.ConnectionConfig.getConnection;
 
+import br.com.desafio.persistence.migration.MigrationStrategy;
+import br.com.desafio.ui.MainMenu;
+
+import java.sql.SQLException;
+
+import static br.com.desafio.persistence.config.ConnectionConfig.getConnection;
+
+
 public class Main {
+
     public static void main(String[] args) throws SQLException {
         try(var connection = getConnection()){
             new MigrationStrategy(connection).executeMigration();
         }
+        new MainMenu().execute();
     }
+
 }
